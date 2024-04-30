9b0039118676d1798050b29da176fafd7e4068c6
elainelovesavocado <weive19Inc@gmail.com>
2024-04-30 14:05:49 -0400

    update ranking apirl

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -36,11 +36,11 @@ function createData(id, name, totalScore, numFirst, numSecond, numSuper, numSupe
 
 const rows = [
   createData(1, 'Jack', 0, 0, 0, 0, 0),
-  createData(2, 'Linyi', 25, 2, 1, 0, 0),
+  createData(2, 'Linyi', 35, 3, 1, 0, 0),
   createData(3, 'Joe Zhou', 25, 2, 1, 0, 0),
   createData(4, 'Elaine', 20, 2, 0, 0, 0),
-  createData(5, 'Joe Sun', 5, 0, 1, 0, 0),
-  createData(6, 'Chloe', 10, 1, 0, 0, 0),
+  createData(5, 'Joe Sun', 15, 1, 1, 0, 0),
+  createData(6, 'Chloe', 20, 1, 2, 0, 0),
   createData(7, 'TJin', 5, 0, 1, 0, 0),
 ];
 
